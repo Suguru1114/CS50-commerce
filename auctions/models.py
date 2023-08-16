c1e9aa5d160a3aabe27d6a8c73d2d11c01768159
Suguru Miyata <suguru1114@gmail.com>
2023-08-16 13:22:58 +1000

    add admin page

--- auctions/models.py
+++ auctions/models.py
@@ -5,12 +5,14 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class listing(models.Model):
+class Listing(models.Model):
     title = models.CharField(max_length=64, unique= True)
     text_discription = models.CharField(max_digits=7,decimal_places=0) 
     price = models.DecimalField() 
 
+    def __str__(self):
+        return self.name
+
 # class bids(models.Model):
 
 # class comments(models.Model):
-test
