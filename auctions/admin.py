3b9782740c574d22154b977bccfb7cd3ed5e0c89
Suguru Miyata <suguru1114@gmail.com>
2023-09-08 16:44:57 +1000

    add superuser

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Listing, Bids
+from .models import User, Listing, Category
 
 # from django.contrib.auth.models import Listing, Bids 
 
@@ -10,5 +10,9 @@ from .models import User, Listing, Bids
     # list_display= ("title","text_description","price")
 
 admin.site.register(User)
-admin.site.register(Listing) 
-admin.site.register(Bids)
+admin.site.register(Category) 
+admin.site.register(Listing)
+
+
+# superuser; suguru
+# password; 12345
