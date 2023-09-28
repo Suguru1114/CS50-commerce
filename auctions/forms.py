c09ee2296006e1212ffd74b76893b33877c5bc12
Suguru Miyata <suguru1114@gmail.com>
2023-09-28 13:44:22 +1000

    adding bids function

--- auctions/forms.py
+++ auctions/forms.py
@@ -8,4 +8,7 @@ class ListingForm(forms.ModelForm):
         fields = ['title', 'text_description', 'price', 'image_url', 'category']
 
 
-        # i might need bids here 
+        # i might need bids here =
+
+class BidForm(forms.Form):
+    bid_amount = forms.DecimalField(max_digits=10, decimal_places=4)
